ac79524444dbb6b636fd578a9fe36d1192e8c81b
Luka Đurišić <luka.durisic123@gmail.com>
2023-06-04 17:22:43 +0200

    new layout of queryes

--- modules/routes/partRoute.js
+++ modules/routes/partRoute.js
@@ -1,14 +1,18 @@
 const express = require("express");
 // const getAllParts = require("../data/getAllParts.js");
 // const getAvailableParts = require("../data/getAvailableParts.js");
-const getPartManufacturer = require("../data/getPartManufacturer");
+const { getPartManufacturer,getAllParts } = require("../data/partQuery");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const data = await getPartManufacturer;
-
   res.send(data);
 });
+
+router.get('/all', async (req, res) => {
+  const data = await getAllParts;
+  res.send(data);
+})
 
 module.exports = router;
