158b94ae600b88e54f8653df0da0fb85207ecb65
Luka Đurišić <luka.durisic123@gmail.com>
2023-07-27 15:39:35 +0200

    added availability check

--- modules/data/userQuery.js
+++ modules/data/userQuery.js
@@ -28,12 +28,15 @@ async function createCart(uid) {
 
 async function addPartInCart(cartid, partid, quantity) {
   try {
-    const addPart = await PartInCart.create({
-      cart_id: cartid,
-      part_id: partid,
-      quantity: quantity,
-    });
-    console.log("part added");
+    const adding = await refreshQuantity(partid, quantity);
+    if(adding != 0){
+      const addPart = await PartInCart.create({
+        cart_id: cartid,
+        part_id: partid,
+        quantity: quantity,
+      });
+      console.log("part added");
+    }
   } catch (err) {
     console.log(err);
   }
@@ -42,10 +45,17 @@ async function addPartInCart(cartid, partid, quantity) {
 async function refreshQuantity(partid, quantity) {
   Part.findByPk(partid).then((prt) => {
     if (prt) {
-      prt.available = prt.available - quantity;
-      prt.save();
+      if (prt.available > quantity) {
+        prt.available = prt.available - quantity;
+        prt.save();
+        return 1;
+      } else {
+        console.log("There are not that many available parts");
+        return 0;
+      }
     } else {
       console.log("User not found!");
+      return 0;
     }
   });
 }
